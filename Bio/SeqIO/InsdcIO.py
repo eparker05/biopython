17e19e1db69a237602bf6c94c142aa4bb4fa48c2
Evan Parker <eparker05@gmail.com>
2014-07-03 15:56:27 -0700

    fixed upper case Seq inconsistency

--- Bio/SeqIO/InsdcIO.py
+++ Bio/SeqIO/InsdcIO.py
@@ -1391,6 +1391,7 @@ class GenbankSeqRecProxy(_lazy.SeqRecordProxyBase):
         firstline = "".join(let for let in firstline if let != " ")
         firstline = firstline[letters_firstline:]
         if len(firstline) >= lengthtoget:
+            firstline = firstline.upper()
             self._seq = Seq(firstline[0:lengthtoget], self._alphabet)
             return
         
@@ -1414,4 +1415,5 @@ class GenbankSeqRecProxy(_lazy.SeqRecordProxyBase):
         print(sequence)
         if len(sequence) != len(self):
             raise ValueError("File not formatted correctly")
+        sequence = sequence.upper()
         self._seq = Seq(sequence, self._alphabet)
